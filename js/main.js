e0a778e807a7e78b7ccb33ca36e70877d421881a
m-hasan-pranto <83342832+m-hasan-pranto@users.noreply.github.com>
2021-11-11 03:08:07 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-// let menu = document.querySelector("#menu-bars");
-// let header = document.querySelector("header");
-
-// menu.onclick = () => {
-//   menu.classList.toggle("fa-times");
-//   header.classList.toggle("active");
-// };
-
-// window.onscroll = () => {
-//   menu.classList.remove("fa-times");
-//   header.classList.remove("active");
-// };
-
-// var typed = new Typed(".typing-text", {
-//   strings: [
-//     "front end developer",
-//     "web designer",
-//     "graphic designer",
-//     "web developer",
-//   ],
-//   loop: true,
-//   typeSpeed: 150,
-// });
-
-// VanillaTilt.init(document.querySelectorAll(".tilt"), {
-//   max: 25,
-// });
 let header = document.querySelector("header");
 let menu = document.querySelector("#menu-bars");
 
@@ -33,6 +6,48 @@ menu.onclick = () => {
   header.classList.toggle("active");
 };
 
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
+};
+
+// typed js
+var typed = new Typed(".typing-text", {
+  strings: ["front-end developer", "web designer", "web developer"],
+  loop: true,
+  typeSpeed: 150,
+});
+
+// tilt js
+
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
   max: 15,
+});
+
+// owlCarousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    dots: true,
+    loop: true,
+    items: 1,
+  });
+});
+
+// scroll To top button
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".scrollToTop").fadeIn();
+    } else {
+      $(".scrollToTop").fadeOut();
+    }
+  });
+
+  $(".scrollToTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
 });
